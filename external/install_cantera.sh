c7bfea92cab8156bbf88d79b9bc7109a5f3b60ee
Yixiao-Zhang <yixiaoz@mit.edu>
2024-12-05 16:01:28 -0500

    Setup on Ubuntu

--- external/install_cantera.sh
+++ external/install_cantera.sh
@@ -2,7 +2,7 @@
 
 CXX=g++
 CC=gcc
-cxx_flags=-std=c++17
+cxx_flags="-std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0"
 prefix=${HOME}/opt/
 python_package=y
 f90_interface=n
@@ -14,10 +14,10 @@ cd cantera
 git submodule init ext/cliboost
 git submodule update ext/cliboost
 
-scons build CXX=${CXX} CC=${CC} cxx_flags=${cxx_flags} prefix=${prefix} \
+scons build CXX=${CXX} CC=${CC} cxx_flags="${cxx_flags}" prefix="${prefix}" \
   python_package=${python_package} f90_interface=${f90_interface} \
   system_eigen=${system_eigen} \
   system_blas_lapack=${system_blas_lapack} \
-  boost_inc_dir=${boost_inc_dir} -j8
+  boost_inc_dir="${boost_inc_dir}" -j8
 scons install
 cd ..
