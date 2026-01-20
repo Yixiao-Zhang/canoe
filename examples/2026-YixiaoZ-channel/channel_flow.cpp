c53286b255fdce2c6130fd52e546349dc0bab9cb
Yixiao-Zhang <yixiaoz@mit.edu>
2026-01-20 17:38:19 -0500

    Allow short channel simulations

--- examples/2026-YixiaoZ-channel/channel_flow.cpp
+++ examples/2026-YixiaoZ-channel/channel_flow.cpp
@@ -205,6 +205,13 @@ inline bool is_boundary(MeshBlock *pmb, const int axis,
 const int i_wall = 1;
 const int i_flow = 2;
 
+enum class ProblemType {
+  LongChannel,
+  NudgedShortChannel,
+};
+
+const ProblemType problem_type = ProblemType::NudgedShortChannel;
+
 const int buffer_size = 1000;
 Real g_ice_temp[buffer_size];
 Real g_evaporation[buffer_size];
@@ -247,7 +254,10 @@ void WallInteraction(MeshBlock *pmb, Real const time, Real const dt,
             u(n, k, j, i) += r * w_kji[n] * w_kji[IDN];
           }
 
-          const Real distance = -get_xv(pmb, i_flow, k, j, i);
+          const Real distance = (
+              (problem_type == ProblemType::LongChannel) ?
+              -get_xv(pmb, i_flow, k, j, i) : 500.
+          );
 
           if (distance > 0) {
             auto solver = WallBoundaryCondition::build_solver(
@@ -362,6 +372,89 @@ void TopSuction(MeshBlock *pmb, Real const time, Real const dt,
   }
 }
 
+int get_mpi_rank(const MPI_Comm mpi_world = MPI_COMM_WORLD) {
+  int rank;
+  MPI_Comm_rank(mpi_world, &rank);
+  return rank;
+}
+
+template<typename F>
+double get_domain_average(F f,MeshBlock *pmb, AthenaArray<Real> const &w) {
+  double local_sum, global_sum;
+  int local_count, global_count;
+
+  local_sum = 0.;
+  local_count = 0;
+  for (int k = pmb->ks; k <= pmb->ke; ++k) {
+    for (int j = pmb->js; j <= pmb->je; ++j) {
+      for (int i = pmb->is; i <= pmb->ie; ++i) {
+        local_sum += f(w.at(k, j, i));
+        ++local_count;
+      }
+    }
+  }
+
+  MPI_Allreduce(&local_sum, &global_sum, 1, MPI_DOUBLE,
+    MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(&local_count, &global_count, 1, MPI_INT,
+    MPI_SUM, MPI_COMM_WORLD);
+
+  return (global_sum / global_count);
+}
+
+Real get_density(StrideIterator<Real*> w) {
+  return w[IDN];
+}
+
+Real get_massflux(StrideIterator<Real*> w) {
+  return w[IDN] * w[IVX + i_flow - 1];
+}
+
+Real get_energy(StrideIterator<Real*> w) {
+  return w[IEN];
+}
+
+void Nudge(MeshBlock *pmb, Real const time, Real const dt,
+                     AthenaArray<Real> const &w, AthenaArray<Real> const &r,
+                     AthenaArray<Real> const &bcc, AthenaArray<Real> &u,
+                     AthenaArray<Real> &s) {
+
+  auto pthermo = Thermodynamics::GetInstance();
+  const int iH2O = pthermo->SpeciesIndex("H2O");
+
+  const Real cv = (
+    pthermo->GetRd() * pthermo->GetCvRatio(iH2O)
+    / (pthermo->GetGammad() - 1.0)
+  );
+
+  const Real mean_density = get_domain_average(get_density, pmb, w);
+  const Real mean_massflux = get_domain_average(get_massflux, pmb, w);
+  const Real mean_energy = get_domain_average(get_energy, pmb, w);
+
+  const static Real prescribed_density = mean_density;
+  const static Real prescribed_massflux = mean_massflux;
+  const static Real prescribed_energy = mean_energy;
+
+  const Real relaxation_rate = 2e-2;
+
+  const Real src_density = relaxation_rate * (
+    prescribed_density - mean_density);
+  const Real src_massflux = relaxation_rate * (
+    prescribed_massflux - mean_massflux);
+  const Real src_energy = relaxation_rate * (
+    prescribed_energy - mean_energy);
+
+  for (int k = pmb->ks; k <= pmb->ke; ++k) {
+    for (int j = pmb->js; j <= pmb->je; ++j) {
+      for (int i = pmb->is; i <= pmb->ie; ++i) {
+        const auto w_kji = w.at(k, j, i);
+        u(IDN, k, j, i) += src_density;
+        u(IVX + i_flow - 1, k, j, i) += src_massflux;
+        u(IEN, k, j, i) += src_energy;
+      }
+    }
+  }
+}
 
 
 void Forcing(MeshBlock *pmb, Real const time, Real const dt,
@@ -369,8 +462,15 @@ void Forcing(MeshBlock *pmb, Real const time, Real const dt,
              AthenaArray<Real> const &bcc, AthenaArray<Real> &u,
              AthenaArray<Real> &s) {
   WallInteraction(pmb, time, dt, w, r, bcc, u, s);
-  BottomInjection(pmb, time, dt, w, r, bcc, u, s);
-  TopSuction(pmb, time, dt, w, r, bcc, u, s);
+
+  if (problem_type == ProblemType::LongChannel) {
+    BottomInjection(pmb, time, dt, w, r, bcc, u, s);
+    TopSuction(pmb, time, dt, w, r, bcc, u, s);
+  } else if (problem_type == ProblemType::NudgedShortChannel) {
+    Nudge(pmb, time, dt, w, r, bcc, u, s);
+  } else {
+    throw std::runtime_error("Unknown problem_type");
+  }
 }
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
@@ -413,24 +513,49 @@ void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin) {
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
+  const auto mesh_size = pmy_mesh->mesh_size;
+  const Real ymin = get_xmin(this, i_wall);
+  const Real ymax = get_xmax(this, i_wall);
+  const Real yc = 0.5 * (ymax + ymin);
+  const Real yd = 0.5 * (ymax - ymin);
+
   auto pthermo = Thermodynamics::GetInstance();
-  auto const water_ice_eos = WaterIceEOS();
-  const Real frac = 0.1;
-  const Real temperature = water_ice_eos.temp3;
-  const Real pressure = frac * water_ice_eos.pres_sat(temperature);
-  const Real density = water_ice_eos.gas.density(temperature, pressure);
+  auto water_ice_eos = WaterIceEOS();
+  const Real temperature = pin->GetReal("initialcondition", "temperature");
+  const Real pressure = water_ice_eos.pres_sat(temperature);
+  const Real density = water_ice_eos.vapor_density_sat(temperature);
+  const Real uc = pin->GetReal("initialcondition", "center_velocity");
 
   // populate to 3D mesh
   for (int k = ks; k <= ke; ++k) {
     for (int j = js; j <= je; ++j) {
       for (int i = is; i <= ie; ++i) {
+        const Real y = get_xv(this, i_wall, k, j, i);
         phydro->w(IDN, k, j, i) = density;
         phydro->w(pthermo->SpeciesIndex("H2O"), k, j, i) = 1.;
+        phydro->w(IVX + i_flow - 1, k, j, i) = uc * (1. - square((y - yc) / yd));
         phydro->w(IPR, k, j, i) = pressure;
       }
     }
   }
 
   peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pcoord, is, ie,
-                             js, je, ks, ke);
+                              js, je, ks, ke);
+
+  std::mt19937 gen(1234 + 17 * get_mpi_rank());
+  std::uniform_real_distribution<Real> phi_distribution(0., 2 * M_PI);
+
+  const Real vp_scale = (
+    pin->GetReal("initialcondition", "velocity_perturbation_scale")
+  );
+
+  for (int k = ks; k <= ke; ++k) {
+    for (int j = js; j <= je; ++j) {
+      for (int i = is; i <= ie; ++i) {
+        const Real phi = phi_distribution(gen);
+        const Real pert = vp_scale * phydro->u(IVX + i_flow - 1, k, j, i);
+        phydro->u(IVX + i_wall - 1, k, j, i) += pert * std::cos(phi);
+      }
+    }
+  }
 }
