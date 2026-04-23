be959942b52c385d95295c7ba7b2766721976b56
Yixiao Zhang <yixiaoz@Yixiaos-MacBook-Pro.local>
2026-04-22 21:33:13 -0400

    Set up a case

---
+++ examples/2026-YixiaoZ-corner/channel_utils.hpp
@@ -0,0 +1,228 @@
+#pragma once
+
+#include <athena/mesh/mesh.hpp>
+#include <mpi.h>
+
+template<typename T>
+auto square(const T x) {
+  return x * x;
+}
+
+template<class Real>
+class IdealGas {
+  public:
+    const Real gas_constant;
+    const Real specific_cv;
+
+    IdealGas(const Real gas_constant, const Real specific_cv):
+      gas_constant(gas_constant), specific_cv(specific_cv) {
+    }
+
+    template<class R1, class R2>
+    inline auto density(const R1 &temp, const R2 &pres) const {
+      return pres / (gas_constant * temp);
+    }
+
+    template<class R1>
+    inline auto specific_internal_energy(const R1 &temp) const {
+      return specific_cv * temp;
+    }
+
+    template<class R1>
+    inline auto specific_enthalpy(const R1 &temp) const {
+      return (specific_cv + gas_constant) * temp;
+    }
+};
+
+template<class Real>
+class CondensedMatter {
+  public:
+    const IdealGas<Real> gas;
+    const Real temp3;
+    const Real pres3;
+    const Real beta;
+    const Real delta;
+
+    CondensedMatter(const IdealGas<Real> &gas,
+        const Real temp3, const Real pres3,
+        const Real beta, const Real delta):
+      gas(gas), temp3(temp3), pres3(pres3), beta(beta), delta(delta) {}
+
+    template<class R1>
+    inline auto specific_internal_energy(const R1 &temp) const {
+      return (
+        gas.specific_enthalpy(temp)
+        + gas.gas_constant * (-beta * temp3 + delta * temp)
+      );
+    }
+
+    template<class R>
+    inline auto pres_sat(const R &temp) const {
+      auto t3 = temp / temp3;
+      return pres3 * exp(beta * (1. - 1./t3) - delta * log(t3));
+    }
+
+    template<class R>
+    inline auto vapor_density_sat(const R &temp) const {
+      return gas.density(temp, pres_sat(temp));
+    }
+};
+
+inline auto WaterIceEOS() {
+  const double Avogadro = 6.02214076e23;
+  const double Boltzmann = 1.380649e-23;
+  const Real atomic_mass_H = 1.008e-3;
+  const Real atomic_mass_O = 15.999e-3;
+  const Real universial_gas_constant = Avogadro * Boltzmann;
+
+  const Real water_mw = 2 * atomic_mass_H  + atomic_mass_O;
+
+  const Real water_gas_constant = universial_gas_constant / water_mw;
+
+  const Real water_vapor_cp_mol = 37.4;
+
+  const Real water_vapor_cp = water_vapor_cp_mol / water_mw;
+
+  const Real water_vapor_cv = water_vapor_cp - water_gas_constant;
+
+  const Real temp3 = 273.16;
+  const Real pres3 = 611.7;
+  const Real beta = 22.46;
+  const Real delta = 0.;
+
+  IdealGas<Real> water_vapor(water_gas_constant, water_vapor_cv);
+  CondensedMatter<Real> water_ice(water_vapor, temp3, pres3, beta, delta);
+  return water_ice;
+}
+
+inline int get_axis_i(const int axis,
+      const int k, const int j, const int i) {
+  switch (axis) {
+    case 1:
+      return i;
+    case 2:
+      return j;
+    case 3:
+      return k;
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+
+inline Real get_xv(MeshBlock *pmb, const int axis,
+      const int k, const int j, const int i) {
+  switch (axis) {
+    case 1:
+      return pmb->pcoord->x1v(i);
+    case 2:
+      return pmb->pcoord->x2v(j);
+    case 3:
+      return pmb->pcoord->x3v(k);
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+inline Real get_dxf(MeshBlock *pmb, const int axis,
+      const int k, const int j, const int i) {
+  switch (axis) {
+    case 1:
+      return pmb->pcoord->dx1f(i);
+    case 2:
+      return pmb->pcoord->dx2f(j);
+    case 3:
+      return pmb->pcoord->dx3f(k);
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+inline Real get_xmin(MeshBlock *pmb, const int axis) {
+  switch (axis) {
+    case 1:
+      return pmb->pmy_mesh->mesh_size.x1min;
+    case 2:
+      return pmb->pmy_mesh->mesh_size.x2min;
+    case 3:
+      return pmb->pmy_mesh->mesh_size.x3min;
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+inline Real get_xmax(MeshBlock *pmb, const int axis) {
+  switch (axis) {
+    case 1:
+      return pmb->pmy_mesh->mesh_size.x1max;
+    case 2:
+      return pmb->pmy_mesh->mesh_size.x2max;
+    case 3:
+      return pmb->pmy_mesh->mesh_size.x3max;
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+inline Real get_x_range(MeshBlock *pmb, const int axis) {
+  return get_xmax(pmb, axis) - get_xmin(pmb, axis);
+}
+
+
+inline bool is_left_boundary(MeshBlock *pmb, const int axis,
+      const int k, const int j, const int i) {
+  return get_xv(pmb, axis, k, j, i) < (
+    get_xmin(pmb, axis) + get_dxf(pmb, axis, k, j, i)
+  );
+}
+
+inline bool is_right_boundary(MeshBlock *pmb, const int axis,
+      const int k, const int j, const int i) {
+  return get_xv(pmb, axis, k, j, i) > (
+    get_xmax(pmb, axis) - get_dxf(pmb, axis, k, j, i)
+  );
+}
+
+inline int get_mpi_rank(const MPI_Comm mpi_world = MPI_COMM_WORLD) {
+  int rank;
+  MPI_Comm_rank(mpi_world, &rank);
+  return rank;
+}
+
+inline Real get_center_flux(const AthenaArray<Real> (&flux)[3], const int axis,
+      const int n, const int k, const int j, const int i) {
+  switch (axis) {
+    case 1:
+      return 0.5 * (flux[X1DIR](n, k, j, i+1) + flux[X1DIR](n, k, j, i));
+    case 2:
+      return 0.5 * (flux[X2DIR](n, k, j+1, i) + flux[X2DIR](n, k, j, i));
+    case 3:
+      return 0.5 * (flux[X3DIR](n, k+1, j, i) + flux[X3DIR](n, k, j, i));
+    default:
+      throw std::runtime_error("Unknown Axis");
+  }
+}
+
+template<typename F>
+double get_domain_average(F f,MeshBlock *pmb, AthenaArray<Real> const &w) {
+  double local_sum, global_sum;
+  int local_count, global_count;
+
+  local_sum = 0.;
+  local_count = 0;
+  for (int k = pmb->ks; k <= pmb->ke; ++k) {
+    for (int j = pmb->js; j <= pmb->je; ++j) {
+      for (int i = pmb->is; i <= pmb->ie; ++i) {
+        local_sum += f(w.at(k, j, i));
+        ++local_count;
+      }
+    }
+  }
+
+  MPI_Allreduce(&local_sum, &global_sum, 1, MPI_DOUBLE,
+    MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(&local_count, &global_count, 1, MPI_INT,
+    MPI_SUM, MPI_COMM_WORLD);
+
+  return (global_sum / global_count);
+}
---
+++ examples/2026-YixiaoZ-corner/corner_flow.cpp
@@ -0,0 +1,242 @@
+#include <athena/athena_arrays.hpp>
+#include <athena/bvals/bvals.hpp>
+#include <athena/coordinates/coordinates.hpp>
+#include <athena/eos/eos.hpp>
+#include <athena/field/field.hpp>
+#include <athena/hydro/hydro.hpp>
+#include <athena/mesh/mesh.hpp>
+#include <athena/parameter_input.hpp>
+
+// climath
+#include <climath/interpolation.h>
+
+// snap
+#include <snap/thermodynamics/atm_thermodynamics.hpp>
+
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include "channel_utils.hpp"
+
+constexpr int i_vapor = 1;
+constexpr int i_solid = 2;
+constexpr int i_flow = 1;
+constexpr int i_norm = 2;
+constexpr int i_span = 3;
+
+constexpr Real x_flow_exit = 0.;
+
+constexpr Real max_nu = 1.;
+
+inline Real get_mu(Real mu, Real rho) {
+  return std::min(mu / rho, max_nu);
+}
+
+inline Real get_cp(const int n_species) {
+  auto pthermo = Thermodynamics::GetInstance();
+  return (
+      pthermo->GetRd() * (
+        (pthermo->GetCvRatio(n_species) / (pthermo->GetGammad() - 1.))
+        + pthermo->GetInvMuRatio(n_species)
+      )
+  );
+}
+
+template<class Real>
+class DensityForcing {
+  public:
+    const int n_species;
+    const Real cp;
+
+    DensityForcing(const int n_species):
+      n_species(n_species), cp(get_cp(n_species)) {}
+
+    inline void apply(StrideIterator<Real*> u, StrideIterator<Real*> w,
+                      const Real drho, const Real source_temp) const {
+        auto pthermo = Thermodynamics::GetInstance();
+        const Real t_exchange = (
+          (drho > 0) ? source_temp
+          : pthermo->GetTemp(w)
+        );
+        const Real u_exchange = (drho > 0) ? 0. : w[IVX];
+        const Real v_exchange = (drho > 0) ? 0. : w[IVY];
+        const Real w_exchange = (drho > 0) ? 0. : w[IVZ];
+
+        u[n_species] += drho;
+        u[IEN] += drho * (
+          cp * t_exchange + 0.5 * (
+            square(u_exchange) + square(v_exchange) + square(w_exchange)
+          )
+        );
+        u[IVX] += drho * u_exchange;
+        u[IVY] += drho * v_exchange;
+        u[IVZ] += drho * w_exchange;
+    }
+};
+
+
+void WaterVaporConduction(HydroDiffusion *phdif, MeshBlock *pmb, const AthenaArray<Real> &prim,
+                     const AthenaArray<Real> &bcc,
+                     int is, int ie, int js, int je, int ks, int ke) {
+  auto pthermo = Thermodynamics::GetInstance();
+
+  const Real gas_cp = get_cp(i_vapor);
+
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      for (int i=is; i<=ie; ++i) {
+        const Real rho = prim(IDN, k, j, i);
+        const Real kappa = get_mu(phdif->kappa_iso, rho);
+        phdif->kappa(HydroDiffusion::DiffProcess::iso, k, j, i) = (
+          kappa * rho * gas_cp
+        );
+      }
+    }
+  }
+  return;
+}
+
+void WaterVaporViscosity(HydroDiffusion *phdif, MeshBlock *pmb, const AthenaArray<Real> &prim,
+                    const AthenaArray<Real> &bcc, int is, int ie, int js, int je,
+                    int ks, int ke) {
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      for (int i=is; i<=ie; ++i) {
+        const Real rho = prim(IDN, k, j, i);
+        const Real nu = get_mu(phdif->nu_iso, rho);
+        phdif->nu(HydroDiffusion::DiffProcess::iso, k, j, i) = (
+          nu
+        );
+      }
+    }
+  }
+  return;
+}
+
+void Mesh::InitUserMeshData(ParameterInput *pin) {
+  auto pthermo = Thermodynamics::GetInstance();
+
+  if (pthermo->SpeciesIndex("H2O") != i_vapor) {
+    throw std::runtime_error("i_vapor does not match");
+  }
+  if (pthermo->SpeciesIndex("H2O(s)") != i_solid) {
+    throw std::runtime_error("i_solid does not match");
+  }
+
+  // const static bool forcing_wall_interaction = pin->GetBoolean("problem",
+  //   "forcing_wall_interaction");
+  // const static bool forcing_bottom_ejection = pin->GetBoolean("problem",
+  //   "forcing_bottom_ejection");
+
+  // auto _forcing = [](MeshBlock *pmb, Real const time, Real const dt,
+  //              AthenaArray<Real> const &w, AthenaArray<Real> const &r,
+  //              AthenaArray<Real> const &bcc, AthenaArray<Real> &u,
+  //              AthenaArray<Real> &s) -> void {
+  //   if (forcing_wall_interaction) {
+  //     // WallInteraction(pmb, time, dt, w, r, bcc, u, s);
+  //   }
+  //   if (forcing_bottom_ejection) {
+  //     // BottomInjection(pmb, time, dt, w, r, bcc, u, s);
+  //   }
+  // };
+
+  // EnrollUserExplicitSourceFunction(_forcing);
+  EnrollViscosityCoefficient(WaterVaporViscosity);
+  EnrollConductionCoefficient(WaterVaporConduction);
+
+  if (pin->GetOrAddString("mesh", "ix1_bc", "none") == "user") {
+    const static Real exit_delta = pin->GetReal(
+      "problem", "exit_delta");
+    const static Real center_velocity = pin->GetReal(
+      "problem", "exit_center_velocity");
+    const static Real temperature = pin->GetReal(
+      "problem", "exit_temperature");
+
+    auto _bottom_bc = [](MeshBlock *pmb, Coordinates *pco,
+                      AthenaArray<Real> &prim, FaceField &b,
+                      Real time, Real dt,
+                      int il, int iu,
+                      int jl, int ju,
+                      int kl, int ku, int ngh) -> void {
+      for (int k = kl; k <= ku; ++k) {
+        for (int j = jl; j <= ju; ++j) {
+          for (int ii = 1; ii <= ngh; ++ii) {
+
+            const int i = il - ii;
+            auto w = prim.at(k, j, i);
+            const Real x = get_xv(pmb, i_norm, k, j, i);
+
+            if (std::abs(x) < exit_delta) {
+              auto water_ice_eos = WaterIceEOS();
+              const Real pressure = water_ice_eos.pres_sat(temperature);
+              const Real density = water_ice_eos.gas.density(
+                temperature, pressure);
+              w[IDN] = density;
+              w[i_vapor] = 1.;
+              w[i_solid] = 0.;
+              for (int n = IVX; n <= IVZ; ++n) {
+                w[n] = 0.;
+              }
+              w[IVX+i_flow-1] += (
+                center_velocity * (1. - square(x/exit_delta)));
+              w[IPR] = pressure;
+            } else {
+              auto wi = prim.at(k, j, il + ii);
+              for (int n = 0; n < NHYDRO; ++n) {
+                const int sign = (IVX <= n && n <= IVZ) ? -1 : 1;
+                w[n] = sign * wi[n];
+              }
+            }
+          }
+        }
+      }
+    };
+    EnrollUserBoundaryFunction(BoundaryFace::inner_x1, _bottom_bc);
+  }
+}
+
+void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
+  AllocateUserOutputVariables(1);
+  SetUserOutputVariableName(0, "temp");
+}
+
+void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin) {
+  auto pthermo = Thermodynamics::GetInstance();
+  auto &w = phydro->w;
+
+  for (int k = ks; k <= ke; ++k) {
+    for (int j = js; j <= je; ++j) {
+      for (int i = is; i <= ie; ++i) {
+        user_out_var(0, k, j, i) = pthermo->GetTemp(w.at(k, j, i));
+      }
+    }
+  }
+}
+
+void MeshBlock::ProblemGenerator(ParameterInput *pin) {
+
+  auto pthermo = Thermodynamics::GetInstance();
+  auto water_ice_eos = WaterIceEOS();
+  const Real temperature = pin->GetReal(
+    "problem", "exit_temperature");
+  const Real outerspace_pressure = pin->GetReal(
+    "problem", "outerspace_pressure");
+  for (int k = ks; k <= ke; ++k) {
+    for (int j = js; j <= je; ++j) {
+      for (int i = is; i <= ie; ++i) {
+        const Real pressure = outerspace_pressure;
+        const Real density = water_ice_eos.gas.density(temperature, pressure);
+        phydro->w(IDN, k, j, i) = density;
+        phydro->w(i_vapor, k, j, i) = 1.;
+        phydro->w(IPR, k, j, i) = pressure;
+      }
+    }
+  }
+
+  peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pcoord, is, ie,
+                              js, je, ks, ke);
+}
